cd8d86668a692df230950c8bce1de64ed0d3583e
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2025-01-05 19:59:49 +0700

    update book isbn

--- src/components/provider/provider.jsx
+++ src/components/provider/provider.jsx
@@ -1,24 +1,25 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Tạo QueryClient với các tùy chọn mặc định
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false, // Không refetch khi focus vào window
-      staleTime: 5 * 60 * 1000, // Thời gian stale là 5 phút
-      retry: false, // Không retry khi query thất bại
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false, // Không refetch khi focus vào window
+            staleTime: 5 * 60 * 1000, // Thời gian stale là 5 phút
+            retry: false, // Không retry khi query thất bại
+        },
     },
-  },
 });
 
 // Component Provider
-export default function ClientProvider({ children }) {
-  return (
-    // Bao bọc children bằng QueryClientProvider để cung cấp queryClient cho toàn bộ ứng dụng
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
-  );
+export default function ClientProvider({children}) {
+    return (
+        // Bao bọc children bằng QueryClientProvider để cung cấp queryClient cho toàn bộ ứng dụng
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
 }
